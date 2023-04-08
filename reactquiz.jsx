669a14c98b40a17ca69ccfb51e68433a830f9886
Kazım Kesler <44920300+kazimkesler@users.noreply.github.com>
2023-04-08 22:30:16 +0300

    Update reactquiz.jsx

--- reactquiz.jsx
+++ reactquiz.jsx
@@ -1,53 +1,35 @@
-
 import React from "react";
 import { useState, useEffect } from "react";
 
 export default function App(props) {
-  const [zar1, setZar1] = useState(1);
-  const [zar2, setZar2] = useState(2);
-  const [win, setWin] = useState(false);
-  const check = () => {
-    setZar1(Math.floor(Math.random() * 7));
-    setZar2(Math.floor(Math.random() * 7));
-  };
+    const [dice1, setDice1] = useState(1);
+    const [dice2, setDice2] = useState(2);
+    const [win, setWin] = useState(false);
 
-  useEffect(() => {
-    if (zar1 === zar2)
-     setWin(true);
-  });
+    const roll = () => {
+        setDice1(Math.floor(Math.random() * 7));
+        setDice2(Math.floor(Math.random() * 7));
+    };
 
-  const imageList=[
-    "https://github.com/londonappbrewery/dicee-flutter/blob/master/images/dice1.png?raw=true",
-    "https://github.com/londonappbrewery/dicee-flutter/blob/master/images/dice2.png?raw=true",
-    "https://github.com/londonappbrewery/dicee-flutter/blob/master/images/dice3.png?raw=true",
-    "https://github.com/londonappbrewery/dicee-flutter/blob/master/images/dice4.png?raw=true",
-    "https://github.com/londonappbrewery/dicee-flutter/blob/master/images/dice5.png?raw=true",
-    "https://github.com/londonappbrewery/dicee-flutter/blob/master/images/dice6.png?raw=true"];
+    useEffect(() => {
+        if (dice1 === dice2)
+            setWin(true);
+    }, [dice1, dice2]);
 
-  return (
-    <div className="App">
-      <h1>
-        zar1: {zar1} 
-        {/*<img  width="50" height="50" src={require(imageList[zar1-1])} alt="" /> */}
-      </h1>
-      <h1>
-        zar2: {zar2}
-        {/*<img  width="50" height="50" src={require(imageList[zar2-1])} alt="" /> */}
-      </h1>
-      {!win ? (
-        <button onClick={() => check()}>
-          Dene
-        </button>
-      ) : (
-        <Win win={win} />
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <h1>zar1: {dice1}</h1>
+            <h1>zar2: {dice2}</h1>
+            {!win
+                ? <button onClick={roll}>
+                    Dene
+                </button>
+                : <Win win={win} />}
+        </div>
+    );
 }
 
-function Win(props) {
-  return <div>Kazandın {props.win}</div>;
+function Win() {
+    return <div>Kazandın</div>;
 }
 
-// Log to console
-console.log("Hello console");
